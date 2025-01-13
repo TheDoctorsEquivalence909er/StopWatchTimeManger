fd53eaeef337e3d8adc5d95eda1f8b5acbe22220
TheDoctorsEquivalence909er <chenrichs0822@gmail.com>
2025-01-12 18:38:29 -0600

    Add hours

--- script.js
+++ script.js
@@ -42,8 +42,7 @@ function start(){
 		newTimeStr = "0".toString().padStart(6, '0');
 		newTimeStr = ""+newTimeStr.slice(0,-4)+":"+newTimeStr.slice(-4,-2)+"'"+newTimeStr.slice(-2)+"''";
 		$('.time').text(newTimeStr);
-		$("#lapBox").html(`<div class="LapDisplay">Lap:1-<span id="LapTime">00:00'00''</span></div><textarea name="Boxtextarea" id="textarea0"></textarea>`)
-		console.log(cookieObj);
+		$("#lapBox").html(`<div class="LapDisplay">(Lap:1)<span id="LapTime">00:00:00'00''</span></div><textarea name="Boxtextarea" id="textarea0"></textarea>`)
 	} else {
 		lapTime = cookieObj.lapTime;
 		TimeFromLastStarted = cookieObj.TimeFromLastStarted; 
@@ -65,10 +64,10 @@ function restart(){
 	numberOfLapes = 2;
 	lastClockTime = 0;
 	timeFromLastLap = 0;
-	newTimeStr = "0".toString().padStart(6, '0');
-	newTimeStr = ""+newTimeStr.slice(0,-4)+":"+newTimeStr.slice(-4,-2)+"'"+newTimeStr.slice(-2)+"''";
+	newTimeStr = "0".toString().padStart(8, '0');
+	newTimeStr =  newTimeStr.slice(0,-6)+":"+newTimeStr.slice(-6,-4)+":"+newTimeStr.slice(-4,-2)+"'"+newTimeStr.slice(-2)+"''";
 	$('.time').text(newTimeStr);
-	$("#lapBox").html(`<div class="LapDisplay">Lap:1-<span id="LapTime">00:00'00''</span></div><textarea name="Boxtextarea" id="textarea1" rows="4" cols="50"></textarea>`)
+	$("#lapBox").html(`<div class="LapDisplay">(Lap:1)<span id="LapTime">00:00:00'00''</span></div><textarea name="Boxtextarea" id="textarea1" rows="4" cols="50"></textarea>`)
 
 	
 	document.cookie = `lapTime=${lapTime}; SameSite=None; Secure; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
@@ -80,7 +79,6 @@ function restart(){
 	document.cookie = `LapBox=${encodeURIComponent($('#lapBox').first().html())}; SameSite=None; Secure; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
 	cookieObj = parseCookies();
 
-	console.log(cookieObj);
 }
 
 setInterval(function() {
@@ -94,57 +92,50 @@ setInterval(function() {
 
 function updateColock(){
 
-		// adds time from when last started 
+	var timerText 	= $('.time').text();
 
-	var timerText = $('.time').text();
+	timerText 		= timerText.replace(/\D/g,'');
 
-	timerText = timerText.replace(/\D/g,'');
+	timerNum 		= parseInt(timerText,10);
 
-	timerNum = parseInt(timerText,10);
+	timerNum 		*= 10;
 
+	timePased 		= Date.now()-TimeFromLastStarted;
+
+	var addTime 	= (timePased-timerNum);
+
+	timerNum 		+= addTime;
+
+	lastClockTime 	= timerNum;
+
+	lapTime 		= timerNum-timeFromLastLap;
+
+	lastLapTime 	= lapTime;
 	
 
-	timerNum *= 10;
+	timerNum 		= Math.floor(timerNum/10);
 
-	timePased = Date.now()-TimeFromLastStarted;
-
-	var addTime = (timePased-timerNum);
-
-	timerNum += addTime;
-
-	lastClockTime = timerNum;
-
-	lapTime = timerNum-timeFromLastLap;
-
-	lastLapTime = lapTime;
-	
-
-	timerNum = Math.floor(timerNum/10);
-
-	lapTime = Math.floor(lapTime/10);
-
-	timerNum =  10000*Math.trunc(timerNum/6000) + timerNum%6000;
-
-	lapTime =  10000*Math.trunc(lapTime/6000) + lapTime%6000;
-
-	console.log(timerNum,10000*Math.trunc(timerNum/6000) + timerNum%6000);
+	lapTime 		= Math.floor(lapTime/10);
 
 
 
-	newTimeStr = timerNum.toString().padStart(6, '0');
-	newTimeStr = ""+newTimeStr.slice(0,-4)+":"+newTimeStr.slice(-4,-2)+"'"+newTimeStr.slice(-2)+"''";
+	timerNum 		= 10000*Math.trunc(timerNum/6000) 		+ timerNum	%6000;
+	timerNum		= 1000000*Math.trunc(timerNum/600000)	+ timerNum	%600000;
+
+	lapTime 		= (10000*Math.trunc(lapTime/6000) 		+ lapTime	%6000);
+	lapTime			= 1000000*Math.trunc(lapTime/600000)	+ lapTime	%600000;
+
+	newTimeStr	 	= timerNum.toString().padStart(8, '0');
+	newTimeStr 		= newTimeStr.slice(0,-6)+":"+newTimeStr.slice(-6,-4)+":"+newTimeStr.slice(-4,-2)+"'"+newTimeStr.slice(-2)+"''";
 
 	$('.time').text(newTimeStr);
-	newLapTimestr = lapTime.toString().padStart(6, '0');
 
-	newLapTimestr = ""+newLapTimestr.slice(0,-4)+":"+newLapTimestr.slice(-4,-2)+"'"+newLapTimestr.slice(-2)+"''";
+	newLapTimestr 	= lapTime.toString().padStart(8, '0');
+
+	newLapTimestr 	= newLapTimestr.slice(0,-6)+":"+newLapTimestr.slice(-6,-4)+":"+newLapTimestr.slice(-4,-2)+"'"+newLapTimestr.slice(-2)+"''";
+
 	$('#LapTime').text(newLapTimestr);
 
-	// lapTime = 0;
-	// TimeFromLastStarted =  new Date();
-	// numberOfLapes = 2;
-	// lastClockTime = 0;
-	// timeFromLastLap = 0;
 	document.cookie = `lapTime=${lapTime}; SameSite=None; Secure; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
 	document.cookie = `TimeFromLastStarted=${TimeFromLastStarted}; SameSite=None; Secure; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
 	document.cookie = `numberOfLapes=${numberOfLapes}; SameSite=None; Secure; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
@@ -212,13 +203,12 @@ $('#lapButton').on('click', function (e) {
 		timeFromLastLap = lastLapTime;
 	
 		// Append the new div to the container
-		$("#lapBox").prepend(`<div class="LapDisplay" id="Lap">Lap:${numberOfLapes}-<span id="LapTime">00:00'00''</span></span></div><textarea name="Boxtextarea" id="textarea${numberOfLapes}" rows="4" cols="50"></textarea>`);
+		$("#lapBox").prepend(`<div class="LapDisplay" id="Lap">(Lap:${numberOfLapes})<span id="LapTime">00:00:00'00''</span></span></div><textarea name="Boxtextarea" id="textarea${numberOfLapes}" rows="4" cols="50"></textarea>`);
 		
 		numberOfLapes++;
 		
 		console.log('lap');
 
 		lapStringBox = $(`#lapBox`).first().html();
-		console.log(lapStringBox);
 	}
 });
